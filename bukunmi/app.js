75770b4d1292b350de04c7dc118970027a5cee87
Kunmi-Dev <aweobukunmi@gmail.com>
2024-05-23 06:16:21 -0700

    Table

---
+++ bukunmi/app.js
@@ -0,0 +1,178 @@
+
+// MULTIPLICATION TABLE 1
+function answerOneBtn(){
+    let numberOne = document.getElementById('numberOne').value
+    let answerOne = document.getElementById('answerOne')
+    var result = 1 * numberOne
+    answerOne.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 2
+function answerTwoBtn(){
+    let numberTwo = document.getElementById('numberTwo').value
+    let answerTwo = document.getElementById('answerTwo')
+    var result = 2 * numberTwo
+    answerTwo.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 3
+function answerThreeBtn(){
+    let numberThree = document.getElementById('numberThree').value
+    let answerThree = document.getElementById('answerThree')
+    var result = 3 * numberThree
+    answerThree.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 4
+function answerFourBtn(){
+    let numberFour = document.getElementById('numberFour').value
+    let answerFour = document.getElementById('answerFour')
+    var result = 4 * numberFour
+    answerFour.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 5
+function answerFiveBtn(){
+    let numberFive = document.getElementById('numberFive').value
+    let answerFive = document.getElementById('answerFive')
+    var result = 5 * numberFive
+    answerFive.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 6
+function answerSixBtn(){
+    let numberSix = document.getElementById('numberSix').value
+    let answerSix = document.getElementById('answerSix')
+    var result = 6 * numberSix
+    answerSix.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 7
+function answerSevenBtn(){
+    let numberSeven = document.getElementById('numberSeven').value
+    let answerSeven = document.getElementById('answerSeven')
+    var result = 7 * numberSeven
+    answerSeven.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 8
+function answerEightBtn(){
+    let numberEight = document.getElementById('numberEight').value
+    let answerEight = document.getElementById('answerEight')
+    var result = 8 * numberEight
+    answerEight.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 9
+function answerNineBtn(){
+    let numberNine = document.getElementById('numberNine').value
+    let answerNine = document.getElementById('answerNine')
+    var result = 9 * numberNine
+    answerNine.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 10
+function answerTenBtn(){
+    let numberTen = document.getElementById('numberTen').value
+    let answerTen = document.getElementById('answerTen')
+    var result = 10 * numberTen
+    answerTen.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 11
+function answerElevenBtn(){
+    let numberEleven = document.getElementById('numberEleven').value
+    let answerEleven = document.getElementById('answerEleven')
+    var result = 11 * numberEleven
+    answerEleven.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 12
+function answerTwelveBtn(){
+    let numberTwelve = document.getElementById('numberTwelve').value
+    let answerTwelve = document.getElementById('answerTwelve')
+    var result = 12 * numberTwelve
+    answerTwelve.textContent = result
+    console.log(result)
+}
+
+
+// MULTIPLICATION TABLE 13
+function answerThirthenBtn(){
+    let numberThirthen = document.getElementById('numberThirthen').value
+    let answerThirthen = document.getElementById('answerThirthen')
+    var result = 13 * numberThirthen
+    answerThirthen.textContent = result
+    console.log(result)
+}
+    
+
+// MULTIPLICATION TABLE 14
+function answerFourthenBtn(){
+    let numberFourthen = document.getElementById('numberFourthen').value
+    let answerFourthen = document.getElementById('answerFourthen')
+    var result = 14 * numberFourthen
+    answerFourthen.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 15
+function answerFifthenBtn(){
+    let numberFifthen = document.getElementById('numberFifthen').value
+    let answerFifthen = document.getElementById('answerFifthen')
+    var result = 15 * numberFifthen
+    answerFifthen.textContent = result
+    console.log(result)
+}
+
+// MULTIPLICATION TABLE 16
+function answerSixthenBtn(){
+    let numberSixthen = document.getElementById('numberSixthen').value
+    let answerSixthen = document.getElementById('answerSixthen')
+    var result = 16 * numberSixthen
+    answerSixthen.textContent = result
+    console.log(result)
+}
+// MULTIPLICATION TABLE 17
+function answerSeventhenBtn(){
+    let numberSeventhen = document.getElementById('numberSeventhen').value
+    let answerSeventhen = document.getElementById('answerSeventhen')
+    var result = 17 * numberSeventhen
+    answerSeventhen.textContent = result
+    console.log(result)
+}
+// MULTIPLICATION TABLE 18
+function answerEighthenBtn(){
+    let numberEighthen = document.getElementById('numberEighthen').value
+    let answerEighthen = document.getElementById('answerEighthen')
+    var result = 18 * numberEighthen
+    answerEighthen.textContent = result
+    console.log(result)
+}
+// MULTIPLICATION TABLE 19
+function answerNinethenBtn(){
+    let numberNinethen = document.getElementById('numberNinethen').value
+    let answerNinethen = document.getElementById('answerNinethen')
+    var result = 19 * numberNinethen
+    answerNinethen.textContent = result
+    console.log(result)
+}
+// MULTIPLICATION TABLE 20
+function answerTwentyBtn(){
+    let numberTwenty = document.getElementById('numberTwenty').value
+    let answerTwenty = document.getElementById('answerTwenty')
+    var result = 20 * numberTwenty
+    answerTwenty.textContent = result
+    console.log(result)
+}
